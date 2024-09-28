1031329b7ce7ed2d3d12bdc6fdec2b70ed79ccf0
dantenvmt <thuannguyen.vm@gmail.com>
2024-09-27 20:56:19 -0500

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -3,6 +3,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
 
 load_dotenv()
 class Chain:
