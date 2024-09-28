5788201dcf9c1fc2d1aeb23c41b2b3034da9462c
dantenvmt <thuannguyen.vm@gmail.com>
2024-09-27 20:17:24 -0500

    Update portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import chromadb
 import uuid
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
 
 class Portfolio:
     def __init__(self, file_path="app/resources/TechStacks_Links.csv"):
