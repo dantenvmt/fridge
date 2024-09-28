9214118847adbe61e5ce528c80694e0f6c7a7b95
dantenvmt <thuannguyen.vm@gmail.com>
2024-09-27 20:55:18 -0500

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -3,12 +3,11 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
-from dotenv import load_dotenv
 
 load_dotenv()
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("api_key"), model_name="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(temperature=0, groq_api_key=st.secrets["api_key"], model_name="llama-3.1-70b-versatile")
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
